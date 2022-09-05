7b614671e459fa6510a42baa0aa306c1cf62e9f4
erieevoicon <thanhtung161094@gmail.com>
2022-09-05 14:36:45 +0700

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path("", views.homePageView, name='home')
-
+    path("", HomePageView.as_view(), name='home'),
+    path("about/", AboutPageView.as_view(), name='about'),
 ]
